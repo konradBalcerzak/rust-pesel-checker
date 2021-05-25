62d4cb4ea43cf558ec22841570598e94042bbb10
Konrad Balcerzak <kobdi4@gmail.com>
2021-05-25 10:39:50 +0200

    Convert Pesel struct to use a vector of digits

--- src/lib.rs
+++ src/lib.rs
@@ -1,60 +1,66 @@
 // use std::env;
 
+struct DigitVec(Vec<u32>);
+
+impl DigitVec {
+    fn new (candidate: &str) -> Result<DigitVec, String> {
+        let mut pesel_digits: Vec<u32> = Vec::new();
+        
+        let candidate_iter = candidate
+            .chars()
+            .map(|char| char.to_digit(10));
+
+        for digit_opt in candidate_iter {
+            if let Some (digit) = digit_opt {
+                pesel_digits.push(digit);
+            } else {
+                return Err(String::from(""));
+            }
+        }
+        Ok(DigitVec(pesel_digits))
+    }
+}
+
 #[derive(Debug)]
 pub enum PeselErr {
     Generic
 }
 
 pub struct Pesel {
-    // value: String
+    value: Vec<u32>
 }
 
 impl Pesel {
     fn new (input: &str) -> Result<Pesel, PeselErr> {
-        if Pesel::check_correctness(input) {
-            return Ok(Pesel {
-                // value: String::from(input)
-            });
+        if let Ok(digits) = DigitVec::new(input) {
+            if Pesel::check_correctness(&digits.0) {
+                return Ok(Pesel {
+                    value: digits.0
+                });
+            }
         }
-        else {
-            return Err(PeselErr::Generic);
-        }
+        return Err(PeselErr::Generic);
     }
     
-    fn check_correctness (candidate: &str) -> bool {
-        if candidate.bytes().count() == 11 {
-            let weights: [u32; 11] = [1, 3, 7, 9, 1, 3, 7, 9, 1, 3, 1];
-            let mut digit_amount = 0;
-            
-            let pesel_digits = candidate
-            .chars()
-            .map(|char| {
-                let digit_opt = char.to_digit(10);
-                if let Some(_) = digit_opt {
-                    digit_amount += 1;
-                }
-                digit_opt
-            });
-            for digit in pesel_digits {
-                println!("{:?}", digit);
-            }
-            let mut sum = 0;
+    fn check_correctness (candidate: &Vec<u32>) -> bool {
+        let weights: [u32; 11] = [1, 3, 7, 9, 1, 3, 7, 9, 1, 3, 1];
 
-            for (offset, char) in candidate[..10].char_indices() {
-                if let Some(digit) = char.to_digit(10) {
-                    sum += digit * weights[offset];
+        if candidate.len() == 11 {
+            let mut sum = 0;
+            let mut iteration = 0;
+
+            for (pesel_digit, multiplier) in candidate.iter().zip(weights.iter()) {
+                if iteration == 10 {
+                    break;
                 }
-                else {
-                    return false;
-                };
+                iteration += 1;
+                sum += pesel_digit * multiplier;
             }
             sum = sum % 10;
             let control_num = 10 - sum;
             
-            if let Some(char) = candidate.chars().last() {
-                if let Some(digit) = char.to_digit(10) {
-                    return digit == control_num || digit == sum;
-                }
+            if let Some(control_digit) = candidate.last() {
+                return *control_digit == control_num || *control_digit == sum;
             }
         }
         false
@@ -70,8 +76,7 @@ pub fn header () -> Result<Pesel, PeselErr> {
         println!("\n================================\n");
         println!("Podaj numer PESEL:");
         if let Ok(_) =  stdin().read_line(&mut input_pesel) {
-            input_pesel = String::from(input_pesel.trim());
-            if let Ok(pesel) = Pesel::new(&input_pesel) {
+            if let Ok(pesel) = Pesel::new(input_pesel.trim()) {
                 return Ok(pesel);
             };
         }
